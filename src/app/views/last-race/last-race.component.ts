ed17e887e82e8138e7fd52c718e88124eb21f959
Andrzej Maćkowski <vandmac@icloud.com>
2022-08-14 12:37:42 +0200

    remove ergast.service  add givenName Constrct name

--- src/app/views/last-race/last-race.component.ts
+++ src/app/views/last-race/last-race.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { LastRaceService } from '../../services/last-race/last-race.service';
-import { ErgastService } from 'src/app/services/ergast.service';
 
 @Component({
   selector: 'app-last-race',
@@ -9,10 +8,9 @@ import { ErgastService } from 'src/app/services/ergast.service';
 })
 export class LastRaceComponent implements OnInit {
   public lastRace$;
- 
+
   constructor(private lastRaceService: LastRaceService) {
     this.lastRace$ = this.lastRaceService.getLastRace();
-
   }
 
   ngOnInit(): void {}
