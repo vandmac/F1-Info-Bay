d7900f467d095ed4f64d4b47351ae881e5815041
Andrzej Maćkowski <vandmac@icloud.com>
2022-08-14 15:35:28 +0200

    display with no table pos, name, constructor,grid

--- src/app/views/last-race/last-race.component.ts
+++ src/app/views/last-race/last-race.component.ts
@@ -1,5 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { LastRaceService } from '../../services/last-race/last-race.service';
+import { MatTableDataSource } from '@angular/material/table';
+
+// export interface PeriodicElement {
+//   familyName: string;
+//   givenName: string;
+//   constructor: string;
+//   grid: number;
+// }
+
+// const ELEMENT_DATA: LastRaceComponent = getLastRace();
 
 @Component({
   selector: 'app-last-race',
@@ -7,10 +17,11 @@ import { LastRaceService } from '../../services/last-race/last-race.service';
   styleUrls: ['./last-race.component.scss'],
 })
 export class LastRaceComponent implements OnInit {
-  public lastRace$;
+    public lastRace$;
 
   constructor(private lastRaceService: LastRaceService) {
     this.lastRace$ = this.lastRaceService.getLastRace();
+    
   }
 
   ngOnInit(): void {}
