ac2c543cc1f7ebee01d4f23c41ff7d166489faff
Andrzej Maćkowski <vandmac@icloud.com>
2022-08-21 11:32:15 +0200

    Finish changes

--- src/app/services/last-race/last-race.ts
+++ src/app/services/last-race/last-race.ts
@@ -109,88 +109,88 @@ export enum Status {
 
 
 
-export interface LastRace {
-  MRData: {
-    xmlns: string;
-    series: string;
-    url: string;
-    limit: number;
-    offset: number;
-    total: number;
-    RaceTable: {
-      season: number;
-      round: number;
-      Races: [
-        {
-          season: number;
-          round: number;
-          url: string;
-          raceName: string;
-          Circuit: {
-            circuitId: string;
-            url: string;
-            circuitName: string;
-            Location: {
-              lat: number;
-              long: number;
-              locality: string;
-              country: string;
-            };
-          };
-          date: string;
-          time: string;
-          Results: [
-            {
-              number: number;
-              position: number;
-              positionText: string;
-              points: number;
-              Driver: {
-                driverId: string;
-                permanentNumber: number;
-                code: string;
-                url: string;
-                givenName: string;
-                familyName: string;
-                dateOfBirth: string;
-                nationality: string;
-              };
-              Constructor: {
-                constructorId: string;
-                url: string;
-                name: string;
-                nationality: string;
-              };
-              grid: number;
-              laps: number;
-              status: string;
-              Time: {
-                millis: number;
-                time: string;
-              };
-              FastestLap: {
-                rank: number;
-                lap: number;
-                Time: {
-                  time: string;
-                };
-                AverageSpeed: {
-                  units: string;
-                  speed: number;
-                };
-              };
-            }
-          ];
-        }
-      ];
-    };
-  };
-}
+// export interface LastRace {
+//   MRData: {
+//     xmlns: string;
+//     series: string;
+//     url: string;
+//     limit: number;
+//     offset: number;
+//     total: number;
+//     RaceTable: {
+//       season: number;
+//       round: number;
+//       Races: [
+//         {
+//           season: number;
+//           round: number;
+//           url: string;
+//           raceName: string;
+//           Circuit: {
+//             circuitId: string;
+//             url: string;
+//             circuitName: string;
+//             Location: {
+//               lat: number;
+//               long: number;
+//               locality: string;
+//               country: string;
+//             };
+//           };
+//           date: string;
+//           time: string;
+//           Results: [
+//             {
+//               number: number;
+//               position: number;
+//               positionText: string;
+//               points: number;
+//               Driver: {
+//                 driverId: string;
+//                 permanentNumber: number;
+//                 code: string;
+//                 url: string;
+//                 givenName: string;
+//                 familyName: string;
+//                 dateOfBirth: string;
+//                 nationality: string;
+//               };
+//               Constructor: {
+//                 constructorId: string;
+//                 url: string;
+//                 name: string;
+//                 nationality: string;
+//               };
+//               grid: number;
+//               laps: number;
+//               status: string;
+//               Time: {
+//                 millis: number;
+//                 time: string;
+//               };
+//               FastestLap: {
+//                 rank: number;
+//                 lap: number;
+//                 Time: {
+//                   time: string;
+//                 };
+//                 AverageSpeed: {
+//                   units: string;
+//                   speed: number;
+//                 };
+//               };
+//             }
+//           ];
+//         }
+//       ];
+//     };
+//   };
+// }
 
-export interface LastRaceService {
-  position: number;
-  givenName: string;
-  familyName: string;
-  constructor: string;
-  grid: number;
-  }
+// export interface LastRaceService {
+//   position: number;
+//   givenName: string;
+//   familyName: string;
+//   constructor: string;
+//   grid: number;
+//   }
--- src/app/views/last-race/last-race.component.ts
+++ src/app/views/last-race/last-race.component.ts
@@ -1,31 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { LastRaceService } from '../../services/last-race/last-race.service';
-import { MatTableDataSource } from "@angular/material/table";
-import { LastRace, Race, } from '../../services/last-race/last-race';
+import { MatTableDataSource } from '@angular/material/table';
+import { LastRace, Race } from '../../services/last-race/last-race';
 
 @Component({
   selector: 'app-last-race',
   templateUrl: './last-race.component.html',
   styleUrls: ['./last-race.component.scss'],
 })
-  
 export class LastRaceComponent implements OnInit {
-
-  constructor (private lastRaceService: LastRaceService) {
+  constructor(private lastRaceService: LastRaceService) {
     this.lastRace$ = this.lastRaceService.getLastRace();
   }
-  displayedColumns: string[] = ['position', 'Driver'];
+  displayedColumns: string[] = ['Position', 'Driver','Constructor'];
   public lastRace$: Observable<Race>;
-  
-  ngOnInit(): void {
-  }
+
+  ngOnInit(): void {}
 }
-
-
- 
-
-
 
 // {{ lastRace.MRData.RaceTable.Races[0].Results[i].Driver.familyName }}
 //                 {{ lastRace.MRData.RaceTable.Races[0].Results[i].Driver.givenName }}
