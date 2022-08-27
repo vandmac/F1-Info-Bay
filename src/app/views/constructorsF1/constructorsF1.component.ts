6b5e8480de1a220e184d3f079e690092dc94a8b2
Andrzej Maćkowski <vandmac@icloud.com>
2022-08-27 10:20:16 +0200

    list ConstructorsF1 correct (SR help)

--- src/app/views/constructorsF1/constructorsF1.component.ts
+++ src/app/views/constructorsF1/constructorsF1.component.ts
@@ -2,26 +2,23 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ConstructorsF1Service } from 'src/app/services/constructorsF1/constructorsF1.service';
 import { MatTableDataSource } from '@angular/material/table';
-import { ConstructorsF1 } from '../../services/constructorsF1/constructorsF1'
+import {
+  Constructor,
+  ConstructorsF1,
+} from '../../services/constructorsF1/constructorsF1';
 
 @Component({
   selector: 'app-constructorsF1',
   templateUrl: './constructorsF1.component.html',
-  styleUrls: ['./constructorsF1.component.scss']
+  styleUrls: ['./constructorsF1.component.scss'],
 })
 export class ConstructorsF1Component implements OnInit {
-
   constructor(private constructorsF1Service: ConstructorsF1Service) {
     this.constructorsF1$ = this.constructorsF1Service.getConstructorsF1();
   }
-  displayedColumns: string[] = [
-    'Name',
-    'Nationality',
-    'url'
-  ];
+  displayedColumns: string[] = ['name', 'nationality', 'url'];
 
-  public constructorsF1$: Observable<ConstructorsF1>;
-  
+  public constructorsF1$: Observable<Constructor[]>;
+
   ngOnInit(): void {}
-  }
-
+}
