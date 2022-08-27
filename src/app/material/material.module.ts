37b93266be8a82b33f4595b93d297e6ab85edc20
Andrzej Maćkowski <vandmac@icloud.com>
2022-08-27 13:01:15 +0200

    start adding filter to constructors

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {MatButtonModule} from "@angular/material/button";
-import {MatIconModule} from "@angular/material/icon";
-import {MatCardModule} from "@angular/material/card";
-import {MatDividerModule} from "@angular/material/divider";
-import {MatProgressBarModule} from "@angular/material/progress-bar";
-import {LayoutModule} from "@angular/cdk/layout";
-import {MatToolbarModule} from "@angular/material/toolbar";
-import {MatSidenavModule} from "@angular/material/sidenav";
-import { MatListModule } from "@angular/material/list";
-import{MatTableModule} from "@angular/material/table";
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatTableModule } from '@angular/material/table';
+import { MatNativeDateModule } from '@angular/material/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [],
@@ -24,7 +29,13 @@ import{MatTableModule} from "@angular/material/table";
     MatButtonModule,
     MatSidenavModule,
     MatListModule,
-    MatTableModule
-  ]
+    MatTableModule,
+    MatNativeDateModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    HttpClientModule
+  ],
 })
-export class MaterialModule { }
+export class MaterialModule {}
--- src/app/views/constructorsF1/constructorsF1.component.ts
+++ src/app/views/constructorsF1/constructorsF1.component.ts
@@ -2,10 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ConstructorsF1Service } from 'src/app/services/constructorsF1/constructorsF1.service';
 import { MatTableDataSource } from '@angular/material/table';
-import {
-  Constructor,
-  ConstructorsF1,
-} from '../../services/constructorsF1/constructorsF1';
+import { Constructor } from '../../services/constructorsF1/constructorsF1';
 
 @Component({
   selector: 'app-constructorsF1',
@@ -13,13 +10,18 @@ import {
   styleUrls: ['./constructorsF1.component.scss'],
 })
 export class ConstructorsF1Component implements OnInit {
-
   public constructorsF1$: Observable<Constructor[]>;
   displayedColumns: string[] = ['name', 'nationality', 'url'];
+  dataSource = new MatTableDataSource(Constructors);
 
   constructor(private constructorsF1Service: ConstructorsF1Service) {
     this.constructorsF1$ = this.constructorsF1Service.getConstructorsF1();
   }
-  
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
   ngOnInit(): void {}
 }
